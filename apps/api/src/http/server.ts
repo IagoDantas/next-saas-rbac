a2f35b986d961df17f96fcea592ddb92d2924e60
IagoDantas <iagodantas.8@gmail.com>
2024-07-17 00:33:28 -0300

    feat: add update organization route

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -22,6 +22,7 @@ import { createOrganization } from './routes/orgs/create-organization'
 import { getMembership } from './routes/orgs/get-membership'
 import { getOrganization } from './routes/orgs/get-organization'
 import { getOrganizations } from './routes/orgs/get-organizations'
+import { updateOrganization } from './routes/orgs/update-organization'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -61,6 +62,7 @@ app.register(getMembership)
 app.register(resetPassword)
 app.register(authenticateWithGithub)
 app.register(getOrganization)
+app.register(updateOrganization)
 app.register(getOrganizations)
 app.register(authenticateWithPassword)
 app.register(fastifyCors)
