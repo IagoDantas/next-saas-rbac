34aa6bbbb3b57ead6c11576931d31d66ab9b5a39
IagoDantas <iagodantas.8@gmail.com>
2024-08-01 22:24:26 -0300

    lint: add comma to permissions

--- packages/auth/src/permissions.ts
+++ packages/auth/src/permissions.ts
@@ -1,10 +1,11 @@
 import { AbilityBuilder } from '@casl/ability'
+
 import { AppAbility } from '.'
 import { User } from './models/user'
 import { Role } from './roles'
 type PermissionByRole = (
   user: User,
-  builder: AbilityBuilder<AppAbility>
+  builder: AbilityBuilder<AppAbility>,
 ) => void
 
 export const permissions: Record<Role, PermissionByRole> = {
