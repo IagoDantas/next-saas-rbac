05629d1efabd6889d53908c7d7c46272e0b6868b
IagoDantas <iagodantas.8@gmail.com>
2024-08-01 22:23:27 -0300

    feat: register update and shutdown route on server

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -22,6 +22,7 @@ import { createOrganization } from './routes/orgs/create-organization'
 import { getMembership } from './routes/orgs/get-membership'
 import { getOrganization } from './routes/orgs/get-organization'
 import { getOrganizations } from './routes/orgs/get-organizations'
+import { shutdownOrganization } from './routes/orgs/shutdown-organization'
 import { updateOrganization } from './routes/orgs/update-organization'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
@@ -63,6 +64,7 @@ app.register(resetPassword)
 app.register(authenticateWithGithub)
 app.register(getOrganization)
 app.register(updateOrganization)
+app.register(shutdownOrganization)
 app.register(getOrganizations)
 app.register(authenticateWithPassword)
 app.register(fastifyCors)
