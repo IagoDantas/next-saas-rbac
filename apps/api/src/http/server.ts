f12c295c734f4d0f0db466cfbb2aac44ea7394e0
IagoDantas <iagodantas.8@gmail.com>
2024-08-01 22:45:23 -0300

    feat: register transfer organization route on server

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -23,6 +23,7 @@ import { getMembership } from './routes/orgs/get-membership'
 import { getOrganization } from './routes/orgs/get-organization'
 import { getOrganizations } from './routes/orgs/get-organizations'
 import { shutdownOrganization } from './routes/orgs/shutdown-organization'
+import { transferOrganization } from './routes/orgs/transfer-organization'
 import { updateOrganization } from './routes/orgs/update-organization'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
@@ -64,6 +65,7 @@ app.register(resetPassword)
 app.register(authenticateWithGithub)
 app.register(getOrganization)
 app.register(updateOrganization)
+app.register(transferOrganization)
 app.register(shutdownOrganization)
 app.register(getOrganizations)
 app.register(authenticateWithPassword)
